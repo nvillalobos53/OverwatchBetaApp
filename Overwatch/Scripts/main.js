f544764f7bb7af21b5c6ebf0752e72d9107b6339
lilbrowntacoboy <robertorodriguezpro@gmail.com>
2016-11-10 20:06:08 -0500

    Add files via upload
    Initial Commit. Nothing extensive so far. Takes input then requests the information from https://api.lootbox.eu/documentation

---
+++ Overwatch/Scripts/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+    // jQuery methods go here...
+    var container = $("#content");
+    var baseURL = "https://api.lootbox.eu/";
+    var json;
+
+    $("#get").click(function () {
+        // console.log("test");
+         var requestUrl = baseURL + $("#platform").val() + "/" + $("#region").val() + "/" + $("#gamerTag").val() + "/profile";
+        // alert(baseURL + $("#platform").val() + "/" + $("#region").val() + "/" + $("#gamerTag").val() + "/profile");
+        $.get(requestUrl,
+            function (data, status) {
+                console.log("test");
+                $.each(data, function(index, item){
+                    $.each(item, function(key, value){
+                        container.append("</br>"+ key + ": " + value + "</br>");
+                    });
+                    container.append("</br></br>");
+                });
+            });
+
+        
+        
+        
+            //  function(data, status){
+            //      $("#avatar").attr("src", data.data.avatar);
+
+            //      console.log(data.data.avatar);
+            //  });
+    })
+});
